1b0706eaf73a46372b3130112086f8330ff14c1d
KleKlai <magallenmaynards@gmail.com>
2023-11-27 21:06:39 +0800

    feat: added drawer and fixing renimated error

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+```sh
+npx create-expo-stack@latest
+
+// Reference: https://github.com/danstepanov/create-expo-stack
+```
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,47 +1,36 @@
-import { Feather } from "@expo/vector-icons";
+import { Feather } from '@expo/vector-icons';
 
-	import React, { useEffect } from "react";
-	import { TamaguiProvider, Button, Text } from "tamagui";
-	import { useFonts } from "expo-font";
-	import { Stack, useRouter, SplashScreen } from "expo-router";
+import React, { useEffect } from 'react';
+import { TamaguiProvider, Button, Text } from 'tamagui';
+import { useFonts } from 'expo-font';
+import { Stack, useRouter, SplashScreen, Slot } from 'expo-router';
 
-	import config from '../tamagui.config'
+import config from '../tamagui.config';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
-	SplashScreen.preventAutoHideAsync();
-
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
-	const router = useRouter();
-	
-		const [loaded] = useFonts({
-			Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
-			InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf")
-		});
+  const router = useRouter();
 
-		useEffect(() => {
-			if (loaded) {
-				SplashScreen.hideAsync();
-			}
-		}, [loaded]);
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
 
-		if (!loaded) return null;
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
-		const BackButton = () => (
-			<Button unstyled flexDirection="row" backgroundColor="transparent" paddingLeft={0} pressStyle={{ opacity: 0.5 }} onPress={router.back} icon={<Feather name="chevron-left" size={16} color="#007AFF" />}><Text color="#007AFF">Back</Text></Button>
-		)
-	
+  if (!loaded) return null;
 
-	return (
-		
-			<TamaguiProvider config={config}>
-		
-		<Stack>
-			<Stack.Screen name="index" options={{ title: "Overview" }} />
-			<Stack.Screen name="details" options={{ title: "Details", headerLeft: () => <BackButton /> }} />
-		</Stack>
-		
-			</TamaguiProvider>
-		
-	);
+  return (
+    <TamaguiProvider config={config}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Slot />
+      </GestureHandlerRootView>
+    </TamaguiProvider>
+  );
 }
-
